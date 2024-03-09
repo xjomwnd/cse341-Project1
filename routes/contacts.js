2cf80f89c489cda8a04bb952888f45600c349d70
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-09 19:32:30 +0300

    COMMIT

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,6 @@
-// routes/contacts.js
 const express = require('express');
 const routes = express.Router();
+const { body, validationResult } = require('express-validator');
 const Contact = require('../models/contact');
 
 // GET all contacts
@@ -13,8 +13,17 @@ routes.get('/', async (req, res) => {
     }
 });
 
-// POST a new contact
-routes.post('/', async (req, res) => {
+// POST a new contact with input validation
+routes.post('/', [
+    body('name').notEmpty().withMessage('Name is required'),
+    body('email').isEmail().withMessage('Invalid email'),
+    body('phone').notEmpty().withMessage('Phone is required')
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     const contact = new Contact({
         name: req.body.name,
         email: req.body.email,
