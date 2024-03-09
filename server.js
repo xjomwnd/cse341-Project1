6ff3d902149ef3220f363db998a5bb8fa908cb57
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-09 22:14:40 +0300

    COMMIT

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 mongoose.connect(config.database.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB');
-        
+
         // Start the Express server after successfully connecting to MongoDB
         app.listen(config.server.port, () => {
             console.log(`Server is running on port ${config.server.port}`);
@@ -17,9 +17,22 @@ mongoose.connect(config.database.mongoURI, { useNewUrlParser: true, useUnifiedTo
     })
     .catch((err) => {
         console.error('Failed to connect to MongoDB:', err);
-        // Exit the process if unable to connect to MongoDB
-        process.exit(1);
+        process.exit(1); // Exit the process if unable to connect to MongoDB
     });
+
+// Middleware for parsing JSON request bodies
+app.use(express.json());
+
+// Middleware for handling CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+});
 
 // Mount the routes
 app.use('/', routes);
