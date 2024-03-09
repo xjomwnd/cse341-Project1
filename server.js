e7102e914d5f8cfa5af2d481ec0131b075a96722
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-09 20:51:10 +0300

    COMMIT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-const config = require('./config'); // Import the config.js file
+const config = require('./config');
 
 const app = express();
 
@@ -9,15 +9,19 @@ const app = express();
 mongoose.connect(config.database.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB');
+        
+        // Start the Express server after successfully connecting to MongoDB
+        app.listen(config.server.port, () => {
+            console.log(`Server is running on port ${config.server.port}`);
+        });
     })
     .catch((err) => {
         console.error('Failed to connect to MongoDB', err);
+        // Exit the process if unable to connect to MongoDB
+        process.exit(1);
     });
 
 // Mount the routes
 app.use('/', routes);
 
-// Start the Express server using the configured port from config.js
-app.listen(config.server.port, () => {
-    console.log(`Server is running on port ${config.server.port}`);
-});
+module.exports = app; // Export the app for testing or further usage
