e40379be08ca517730ab5066601777e2c539762f
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-10 21:27:45 +0300

    commit

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     // Database configuration
     database: {
         // MongoDB connection string
-        mongoURI: process.env.MONGODB_URI || 'mongodb://localhost:27017/mongodb://Joemongo:7Mwathani77@localhost:27017/cse341-project1',
+        mongoURI: process.env.MONGODB_URI || 'mongodb://localhost:27017/mongodb://"Joemongo":"7Mwathani77"@localhost:27017/cse341-project1',
     },
     // Server configuration
     server: {
