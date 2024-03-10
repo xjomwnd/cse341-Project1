6ed92b576eee48d1d80832ca3ca1c8bfe5bd8a5c
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-10 08:10:23 +0300

    COMMIT

--- db/connect.js
+++ db/connect.js
@@ -3,18 +3,17 @@ const mongoose = require('mongoose');
 // Define the MongoDB connection URL
 const mongoURI = 'mongodb://localhost/contact-api';
 
-// Function to establish MongoDB connection
-const connectToMongoDB = async () => {
-    try {
-        await mongoose.connect(mongoURI);
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error('Failed to connect to MongoDB', err);
-    }
-};
-
 // Call the function to connect to MongoDB
 connectToMongoDB();
 
 // Export the mongoose connection
 module.exports = mongoose.connection;
+//
+const encodedPassword = encodeURIComponent('7Mwathani77');
+const databaseUrl = process.env.DATABASE_URL.replace('${encodedPassword}', encodedPassword);
+
+mongoose.connect(databaseUrl, {
+  // Any additional options for mongoose.connect()
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('Failed to connect to MongoDB', err));
