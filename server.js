5e01c2eb04ceabb680d3ea97b49f2f91fa8263fa
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-11 21:27:18 +0300

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongodb = require('mongodb');
 const app = express();
 app.use(bodyParser.json());
 // MongoDB connection string
-const connectionString = 'mongodb://joemongo:7Mwathani77@your-mongodb-host:27017/';
+const connectionString = 'mongodb://joemongo:7Mwathani77@your-mongodb-host:27017/cse341-project1';
 // Connect to MongoDB
 mongodb.MongoClient.connect(connectionString, (err, client) => {
   if (err) {
