10f271ab74281b210c32f2fc891a1aadf93a97b3
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-11 20:16:59 +0300

    commit

--- server.js
+++ server.js
@@ -9,13 +9,9 @@ require('dotenv').config();
 // Now you can access environment variables using process.env
 const PORT = process.env.PORT || 8080;
 
-// Set the MongoDB environment variables
-process.env.MONGODB_USERNAME = 'joemongo';
-process.env.MONGODB_PASSWORD = '7Mwathani77';
-
 // Connect to MongoDB
-const mongoUsername = process.env.MONGODB_USERNAME;
-const mongoPassword = process.env.MONGODB_PASSWORD;
+const mongoUsername = 'joemongo';
+const mongoPassword = '7Mwathani77';
 const mongoConnectionString = `mongodb://${mongoUsername}:${encodeURIComponent(mongoPassword)}@localhost:27017/cse341-project1`;
 
 MongoClient.connect(mongoConnectionString, {
